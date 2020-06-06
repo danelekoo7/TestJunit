9514467b531be456065a2a9c9b3a16206a566f49
danelekoo7 <danieljedrus@gmail.com>
2020-06-06 21:25:04 +0200

    * TDD * adjusted generatePrimeFactors method

--- src/main/java/pl/jedrus/tdd/Factors.java
+++ src/main/java/pl/jedrus/tdd/Factors.java
@@ -1,13 +1,18 @@
 package pl.jedrus.tdd;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Factors {
 
     public static List<Integer> generatePrimeFactors(Integer n) {
-
-
-        return null;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 
 }
