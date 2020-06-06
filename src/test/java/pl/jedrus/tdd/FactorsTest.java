2c2156b666d1f925677e04a9bc13c931fd593483
danelekoo7 <danieljedrus@gmail.com>
2020-06-06 21:27:14 +0200

    * TDD * corrected test and added new one

--- src/test/java/pl/jedrus/tdd/FactorsTest.java
+++ src/test/java/pl/jedrus/tdd/FactorsTest.java
@@ -9,13 +9,22 @@ import static org.junit.Assert.assertEquals;
 public class FactorsTest {
 
     @Test
-    public void givenTwoWhenGeneratePrimeFactorsThenReturnOneTwo() {
+    public void givenTwoWhenGeneratePrimeFactorsThenReturnTwo() {
         Integer number = 2;
-        List<Integer> expected = List.of(1, 2);
+        List<Integer> expected = List.of(2);
 
         List<Integer> result = Factors.generatePrimeFactors(number);
 
         assertEquals(expected, result);
+    }
 
+    @Test
+    public void given10WhenGeneratePrimeFactorsThenReturnTwoFive() {
+        Integer number = 10;
+        List<Integer> expected = List.of(2,5);
+
+        List<Integer> result = Factors.generatePrimeFactors(number);
+
+        assertEquals(expected, result);
     }
 }
