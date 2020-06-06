9adf6ab948477d3de1fad70724a4da15493f03aa
danelekoo7 <danieljedrus@gmail.com>
2020-06-06 21:47:59 +0200

    * TDD * added new tests

--- src/test/java/pl/jedrus/tdd/FactorsTest.java
+++ src/test/java/pl/jedrus/tdd/FactorsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class FactorsTest {
 
@@ -26,5 +27,35 @@ public class FactorsTest {
         List<Integer> result = Factors.generatePrimeFactors(number);
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void given100WhenGeneratePrimeFactorsThenReturnTwoFive() {
+        Integer number = 100;
+        List<Integer> expected = List.of(2,5);
+
+        List<Integer> result = Factors.generatePrimeFactors(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void given228WhenGeneratePrimeFactorsThenReturnTwoThreeNineteen() {
+        Integer number = 228;
+        List<Integer> expected = List.of(2,3,19);
+
+        List<Integer> result = Factors.generatePrimeFactors(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void given228WhenGeneratePrimeFactorsThenReturnTwoThreeNineteenUnordered() {
+        Integer number = 228;
+        List<Integer> expected = List.of(2,19,3);
+
+        List<Integer> result = Factors.generatePrimeFactors(number);
+
+        assertNotEquals(expected, result);
     }
 }
