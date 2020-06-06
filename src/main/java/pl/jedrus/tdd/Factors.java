ae05da0b9ce51749306828e84e2ed63b85cfa64b
danelekoo7 <danieljedrus@gmail.com>
2020-06-06 21:39:07 +0200

    * TDD * corrected generatePrimeFactors

--- src/main/java/pl/jedrus/tdd/Factors.java
+++ src/main/java/pl/jedrus/tdd/Factors.java
@@ -7,12 +7,19 @@ public class Factors {
 
     public static List<Integer> generatePrimeFactors(Integer n) {
         List<Integer> result = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
+            boolean isPrimeNumber = true;
             if (n % i == 0) {
-                result.add(i);
+                for (int j =2; j <= i/2; j++) {
+                    if (i % j == 0) {
+                        isPrimeNumber = false;
+                    }
+                }
+                if (isPrimeNumber) {
+                    result.add(i);
+                }
             }
         }
         return result;
     }
-
 }
