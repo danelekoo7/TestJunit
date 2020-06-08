5a43c581e3c2354687fbd99963f9aeddfa19df8b
danelekoo7 <danieljedrus@gmail.com>
2020-06-08 15:51:38 +0200

    * TDD * adjusted method to calculate circle area and created new tests

--- src/main/java/pl/jedrus/tdd/Circle.java
+++ src/main/java/pl/jedrus/tdd/Circle.java
@@ -1,12 +1,26 @@
 package pl.jedrus.tdd;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
 public class Circle {
 
-    public double circleArea(int r) throws IllegalArgumentException {
+    public double circleArea(int r) throws IllegalArgumentException, ParseException {
         if (r < 0) {
             throw new IllegalArgumentException("n must be positive");
+        } else if (r == 0) {
+            return 0.0;
         }
 
-        return 0.0;
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        double result = Math.PI * r * r;
+        String formatValue = df.format(result);
+
+        double cos = (double) df.parse(formatValue);
+
+        return cos;
     }
 }
--- src/test/java/pl/jedrus/tdd/CircleTest.java
+++ src/test/java/pl/jedrus/tdd/CircleTest.java
@@ -2,19 +2,25 @@ package pl.jedrus.tdd;
 
 import org.junit.Test;
 
+import java.text.ParseException;
+
 import static org.junit.Assert.assertEquals;
 
 public class CircleTest {
 
     @Test
-    public void given2WhenCircleAreaCalculateThenReturn12Point57() {
-        int n = 2;
-        double expected = 12.57;
-
+    public void givenNumberToCalculateWhenCircleAreaCalculateThenReturnCorrectValue() throws ParseException {
         Circle circle = new Circle();
-        double result = circle.circleArea(n);
+
+        assertEquals(12.57, circle.circleArea(2),0);
+        assertEquals(28.28, circle.circleArea(3),0);
+        assertEquals(0, circle.circleArea(0),0);
+    }
 
 
-        assertEquals(result, expected,0.01);
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNumberIsLowerThanZeroThenThrowException() throws ParseException {
+        Circle circle = new Circle();
+        circle.circleArea(-5);
     }
 }
